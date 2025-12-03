f9ee233bc7a816f78c74efc1ff282564a92a8d60
Gary White <gzw@pm.me>
2025-12-03 10:29:49 +0000

    Simplified.

--- src/day_01_p1.clj
+++ src/day_01_p1.clj
@@ -7,24 +7,25 @@
 (defn rotation->direction
   [r]
   (let [dir (first r)
-        num (parse-long (clojure.string/trim (apply str (rest r))))]
+        num (parse-long (apply str (rest r)))]
     (if (= dir \L) (- num) num)))
 
 (defn compute-next-position
   [prev rot]
   (let [dir (rotation->direction rot)
         pos (mod (+ prev dir) 100)]
-    (prn prev rot dir pos)
     pos))
 
 (defn rotate
   [input]
   (->> input
        (reduce
-         (fn [acc rot] (conj acc (compute-next-position (last acc) rot)))
-         [50])))
+         (fn [acc rot] (let [pos (compute-next-position (first acc) rot)]
+                         (if (zero? pos)
+                           [pos (inc (last acc))]
+                           [pos (last acc)])))
+         [50 0])))
 
 (defn solve []
-  (->> (rotate (read-file "resources/test-input.txt"))
-       (filter zero?)
-       count))
+  (->> (rotate (read-file "resources/input.txt"))
+       last))
