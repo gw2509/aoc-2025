757b55594502489453d86e2217661b0b689e0c9a
Gary White <gzw@pm.me>
2025-12-03 10:02:34 +0000

    Simplified a little.

--- src/day_01_p1.clj
+++ src/day_01_p1.clj
@@ -1,15 +1,4 @@
-(ns day-01)
-
-(def test-input ["L68"
-                 "L30"
-                 "R48"
-                 "L5"
-                 "R60"
-                 "L55"
-                 "L1"
-                 "L99"
-                 "R14"
-                 "L82"])
+(ns day-01-p1)
 
 (defn read-file [f]
   (-> (slurp f)
@@ -19,47 +8,23 @@
   [r]
   (let [dir (first r)
         num (parse-long (clojure.string/trim (apply str (rest r))))]
-    (if (= dir \L)
-      (unchecked-negate num)
-      num)))
-
-(defn compute-passes
-  [prev pos dir]
-  (if (and (neg? dir) (neg? pos))
-    (let [passes (abs (quot pos 100))]
-      (if (or (zero? prev) (zero? (mod pos 100)))
-        passes
-        (inc passes)))
-    (if (and (pos? dir) (> pos 100))
-      (let [passes (quot pos 100)]
-        (if (zero? prev)
-          passes
-          passes)))))
+    (if (= dir \L) (- num) num)))
 
 (defn compute-next-position
   [prev rot]
   (let [dir (rotation->direction rot)
-        pos (+ prev dir)
-        pas (or (compute-passes prev pos dir) 0)]
-    (prn prev rot dir pos pas)
-    [pos pas]))
+        pos (mod (+ prev dir) 100)]
+    (prn prev rot dir pos)
+    pos))
 
 (defn rotate
   [input]
   (->> input
        (reduce
-         (fn [acc rot] (let [[pos passes] (compute-next-position (last acc) rot)]
-                         (-> acc
-                             (into (repeat passes 0))
-                             (conj (mod pos 100)))))
+         (fn [acc rot] (conj acc (compute-next-position (last acc) rot)))
          [50])))
 
 (defn solve []
-  (->> (rotate (read-file "resources/input.txt"))
+  (->> (rotate (read-file "resources/test-input.txt"))
        (filter zero?)
        count))
-
-
-
-
-
